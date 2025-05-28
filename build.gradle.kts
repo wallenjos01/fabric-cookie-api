9eadfdd2efe169a81a5d5f85135a5e5f240d78de
wallenjos01 <wallenjos01@gmail.com>
2025-05-28 09:27:16 -0600

    Build against 1.21.6-pre1

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,5 +15,5 @@ dependencies {
 
     compileOnly(libs.jetbrains.annotations)
 
-    modImplementation("org.wallentines:databridge:0.7.0")
+    modImplementation("org.wallentines:databridge:0.8.2-SNAPSHOT")
 }
