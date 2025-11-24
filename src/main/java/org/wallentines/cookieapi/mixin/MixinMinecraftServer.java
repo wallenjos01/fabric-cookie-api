d323f1bfb314f6e76ea1fa5795195c7430e7a049
wallenjos01 <wallenjos01@gmail.com>
2025-11-23 18:43:40 -0700

    0.1.2 Release

--- src/main/java/org/wallentines/cookieapi/mixin/MixinMinecraftServer.java
+++ src/main/java/org/wallentines/cookieapi/mixin/MixinMinecraftServer.java
@@ -6,7 +6,7 @@ import net.fabricmc.fabric.api.event.EventFactory;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.Services;
 import net.minecraft.server.WorldStem;
-import net.minecraft.server.level.progress.ChunkProgressListenerFactory;
+import net.minecraft.server.level.progress.LevelLoadListener;
 import net.minecraft.server.packs.repository.PackRepository;
 import net.minecraft.world.level.storage.LevelStorageSource;
 import org.spongepowered.asm.mixin.Implements;
@@ -29,7 +29,7 @@ public class MixinMinecraftServer {
     private Event<ConfigurationCookieEvents.RequestCookies> cookieapi$serverEvent;
 
     @Inject(method="<init>", at=@At("TAIL"))
-    private void onInit(Thread thread, LevelStorageSource.LevelStorageAccess levelStorageAccess, PackRepository packRepository, WorldStem worldStem, Proxy proxy, DataFixer dataFixer, Services services, ChunkProgressListenerFactory chunkProgressListenerFactory, CallbackInfo ci) {
+    private void onInit(Thread thread, LevelStorageSource.LevelStorageAccess levelStorageAccess, PackRepository packRepository, WorldStem worldStem, Proxy proxy, DataFixer dataFixer, Services services, LevelLoadListener levelListener, CallbackInfo ci) {
         cookieapi$serverEvent = EventFactory.createArrayBacked(ConfigurationCookieEvents.RequestCookies.class, listeners -> (server, packetListener) -> {
             for(ConfigurationCookieEvents.RequestCookies cookie : listeners) {
                 cookie.request(server, packetListener);
